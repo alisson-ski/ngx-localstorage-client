4e53836f5132b75061e831ac25d6933a36d8cb13
Alisson Lewinski <lissluc7@gmail.com>
2024-02-14 16:44:51 -0300

    feat(client): add post function

--- projects/ngx-localstorage-client/src/lib/ngx-localstorage-client.service.ts
+++ projects/ngx-localstorage-client/src/lib/ngx-localstorage-client.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class LocalStorageClient {
 
   constructor() { }
+
+  post(table: string, obj: any) {
+    const dataSet: any[] = JSON.parse(localStorage.getItem(table) || '[]');
+
+    let nextId = 1;
+    if (dataSet.length) {
+      const highestIdItem = dataSet.reduce((prev, current) => {
+        return (prev.id > current.id) ? prev : current;
+      });
+
+      nextId = highestIdItem.id + 1;
+    }
+
+    obj.id = nextId;
+    dataSet.push(obj);
+    localStorage.setItem(table, JSON.stringify(dataSet));
+  }
 }
